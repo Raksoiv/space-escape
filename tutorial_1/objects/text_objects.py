e5c559317bb1e8855a90a4e24102508099fa3512
Oscar Rencoret <oscar.rencoret@unholster.com>
2020-05-21 19:10:59 -0400

    textSprite update

--- tutorial_1/objects/text_objects.py
+++ tutorial_1/objects/text_objects.py
@@ -10,7 +10,13 @@ class TextObject(Sprite):
         font_size=16, antialias=False,
     ):
         super().__init__()
-        self.text = text
-        self.font = Font(font_filename, font_size)
-        self.image = self.font.render(self.text, antialias, font_color)
+        self.font_color = font_color
+        self.font_size = font_size
+        self.antialias = antialias
+        self.font = Font(font_filename, self.font_size)
+        self.image = self.font.render(
+            text,
+            self.antialias,
+            self.font_color
+        )
         self.rect = self.image.get_rect()
